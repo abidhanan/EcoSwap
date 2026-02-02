c671386343e2245ad872546f85b3688a3e85268e
Abid Hanan <abidhanan0904@gmail.com>
2026-02-02 12:34:30 +0700

    Update

--- Public/Views/guest/login.php
+++ Public/Views/guest/login.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Assets/css/auth/login.css">
-    <title>Masuk ke Akun</title>
+    <title>EcoSwap - Masuk</title>
+    <link rel="icon" type="image/png" href="../../Assets/img/auth/logo.png">
     
 </head>
 <body>
--- Public/Views/guest/register.php
+++ Public/Views/guest/register.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../Assets/css/auth/register.css">
-    <title>Daftar Akun Baru</title>
+    <title>EcoSwap - Daftar</title>
+    <link rel="icon" type="image/png" href="../../Assets/img/auth/logo.png">
     
 <body>
     <div class="auth-container">
